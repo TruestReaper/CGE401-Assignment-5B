d6748bad33a3da10ff3a6763b3b8a55996650490
TruestReaper <chirayiljohnny@gmail.com>
2024-10-27 14:00:37 -0500

    Completed First and Third Person Scene

---
+++ Assignment 5B/Assets/Scipts/Target.cs
@@ -0,0 +1,41 @@
+/*
+* Name: John Chirayil
+* CGE401 - Assignment 5B
+* File: Target.cs
+* Desc: Sets the health system for the targets.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
